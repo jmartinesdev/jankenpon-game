84a02ffaf04765c76e2c20b7692ef83c28cee457
Jakeline Martines <jakeline_13_martines@hotmail.com>
2023-06-11 14:31:08 +0100

    fixed indentation and long lines

--- run.py
+++ run.py
@@ -5,9 +5,11 @@ player_victory = 0
 
 
 def player_mov():
+    
     """
     Here the player's choices between rock, paper and scissors will be defined.
     """
+    
     player_choice = input("Choose one: Rock, Paper or Scissors: ")
     if player_choice in ['ROCK', 'Rock', 'rock']:
         player_choice = 'pedra'
@@ -21,17 +23,21 @@ def player_mov():
     return player_choice
 
 def cpu_mov():
+    
     """""
     The machine choice an option to player
     """""
+    
     cpu_choice = choice(['rock', 'paper', 'scissor'])
     return cpu_choice
 
 while True:
+    
     """
     Added looping structure. 
     The game will repeat as long as the player wants to play.
     """
+    
     print('---------')
     
     cpu_choice = cpu_mov()
@@ -39,35 +45,48 @@ while True:
     
     print('---------')
     
-    # structure if add to the player win
-    if (player_choice == 'rock' and cpu_choice == 'scissor') or (player_choice == 'scissor' and cpu_choice == 'paper') or (player_choice == 'paper' and cpu_choice == 'rock'):
-        print(f'Player choose {player_choice} and the machine choose {cpu_choice}. Result: You win!')
-        player_victory += 1 
+    """
+    structure if add to the player win
+    """
+    
+    if (player_choice == 'rock' and cpu_choice == 'scissor') or \
+        (player_choice == 'scissor' and cpu_choice == 'paper') or \
+        (player_choice == 'paper' and cpu_choice == 'rock'):
+    print(f'Player choose {player_choice} and the machine choose '
+          f'{cpu_choice}. Result: You lost!')
+    player_victory += 1
+
         
     # structure elif add to both players draw
+    
     elif player_choice == cpu_choice:
-        print(f'Player choose {player_choice} and the machine choose {cpu_choice}. Result: Players draw!')
+        print(f'Player choose {player_choice} and the machine choose'
+        f'{cpu_choice}.' 'Result: Players draw!')
     
     else: 
-    # strucute else add to the machine wins
-        print(f'Player choose {player_choice} and the machine choose {cpu_choice}. Result: You lost!')
+        print(f'Player choose {player_choice} and the machine choose' 
+        f'{cpu_choice}.' 'Result: You lost!')
         cpu_victory += 1
         
     print('----------')
+    
     """
     add a print to show the player 
     on the screen the number of wins
     """
+    
     print(f"Total players victories: {player_victory}")
     print(f"Total machine victories: {cpu_victory}")
      
     print('-----------')  
     
     player_choice = input('Do you want play again? (y/n)')
+    
     """
     Here we are going to check the while structure for 
     the player to choose whether to continue playing or not.
     """
+    
     if player_choice in ['yes', 'YES', 'Yes', 'y', 'Y']:
         pass
     elif player_choice in ['NO', 'no', 'No', 'n', 'N']:
